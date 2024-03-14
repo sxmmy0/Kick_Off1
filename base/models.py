b4ff5c6286c6e3bf9e134efd4ec91b5dc3c50e48
Samuel Obembe <SamObembe97@gmail.com>
2024-03-14 17:34:49 +0000

    created a form landing page and edited CSS

--- base/models.py
+++ base/models.py
@@ -1,10 +1,16 @@
 from django.db import models
+from modelcluster.fields import ParentalKey
 from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
     PublishingPanel,
+    FieldRowPanel,
+    InlinePanel,
 )
+
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
@@ -20,6 +26,8 @@ from wagtail.models import (
     RevisionMixin,
     TranslatableMixin,
 )
+
+from wagtail.contrib.forms.panels import FormSubmissionsPanel
 
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
@@ -75,3 +83,25 @@ class FooterText(
 
     class Meta(TranslatableMixin.Meta):
         verbose_name_plural = "Footer Text"
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FormSubmissionsPanel(),
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address'),
+                FieldPanel('to_address'),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
+    ]
