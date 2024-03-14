693419b5ddcee40f4bd567ac3188da53cbd231a4
Samuel Obembe <SamObembe97@gmail.com>
2024-03-14 15:29:01 +0000

    Added new model, footer, header, made changes to base.py

--- base/models.py
+++ base/models.py
@@ -3,11 +3,32 @@ from modelcluster.models import ClusterableModel
 from wagtail.admin.panels import (
     FieldPanel,
     MultiFieldPanel,
+    PublishingPanel,
 )
 from wagtail.contrib.settings.models import (
     BaseGenericSetting,
     register_setting,
 )
+
+# import RichTextField:
+from wagtail.fields import RichTextField
+
+# import DraftStateMixin, PreviewableMixin, RevisionMixin, TranslatableMixin:
+from wagtail.models import (
+    DraftStateMixin,
+    PreviewableMixin,
+    RevisionMixin,
+    TranslatableMixin,
+)
+
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    register_setting,
+)
+
+# import register_snippet:
+from wagtail.snippets.models import register_snippet
+
 
 @register_setting
 class NavigationSettings(BaseGenericSetting):
@@ -25,3 +46,32 @@ class NavigationSettings(BaseGenericSetting):
             "Social settings",
         )
     ]
+
+# ...keep the definition of the NavigationSettings model and add the FooterText model:
+@register_snippet
+class FooterText(
+    DraftStateMixin,
+    RevisionMixin,
+    PreviewableMixin,
+    TranslatableMixin,
+    models.Model,
+):
+
+    body = RichTextField()
+
+    panels = [
+        FieldPanel("body"),
+        PublishingPanel(),
+    ]
+
+    def __str__(self):
+        return "Footer text"
+
+    def get_preview_template(self, request, mode_name):
+        return "base.html"
+
+    def get_preview_context(self, request, mode_name):
+        return {"footer_text": self.body}
+
+    class Meta(TranslatableMixin.Meta):
+        verbose_name_plural = "Footer Text"
