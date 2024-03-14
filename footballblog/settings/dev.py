d3607cdc05db5308a82d8bdba94207a4db091f2e
Samuel Obembe <SamObembe97@gmail.com>
2024-03-14 12:27:33 +0000

    New Repo

---
+++ footballblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-5(v!gstjs)@poljr3xj*$mmqsx)h9fnt2@&8=nb-ca*ao*k#cc"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
